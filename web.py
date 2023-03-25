6d633b5ba7386e8eeca9d1194d41253e83e854e5
chas_howard <chashowiii@gmail.com>
2023-03-25 10:55:16 -0500

    Initial Commit

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
-def add_todo():lkjsdf
+def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
